8c67fd09505715192e7972eea3b28dbf5baaefda
Sudam Ranasinghe <sudamranasinghe98@gmail.com>
2024-09-17 17:38:36 +0530

    Creating reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -31,3 +31,22 @@ export const createStream = (formValues) => async (dispatch) => {
   const response = await streams.post("/streams", formValues);
   dispatch({ type: STREAM_CREATE, payload: response.data });
 };
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.post(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.post("/streams");
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
+export const editStream = (id) => async (dispatch) => {
+  const response = streams.put(`/streams/${id}`);
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
